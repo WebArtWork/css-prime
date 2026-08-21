5f3d0e5876cf9deefcf9f5efdfb80e78ea970b5f
Honchar Denys <crackeraki@gmail.com>
2026-08-21 08:33:08 +0300

    fix: update base branch to master in config.json and adjust eslint rules for JavaScript files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,13 +8,45 @@ export default tseslint.config([
         ignores: ['**/dist/**', '**/node_modules/**', '**/umd/**', '**/tokens/**']
     },
     {
+        linterOptions: {
+            reportUnusedDisableDirectives: 'error'
+        }
+    },
+    {
         files: ['**/__tests__/**/*', '**/*.test.*'],
         extends: [jest.configs['flat/recommended']],
         rules: {}
     },
     {
-        files: ['**/*.{js,mjs,ts,mts,d.ts}'],
+        files: ['**/*.{ts,mts,d.ts}'],
         extends: [eslint.configs.recommended, tseslint.configs.recommended, prettierConfig],
+        rules: {
+            'no-console': 'error',
+            'no-debugger': 'error',
+            'no-fallthrough': 'off',
+            'padding-line-between-statements': [
+                'error',
+                { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+                { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+                { blankLine: 'any', prev: ['case', 'default'], next: 'break' },
+                { blankLine: 'any', prev: 'case', next: 'case' },
+                { blankLine: 'always', prev: '*', next: 'return' },
+                { blankLine: 'always', prev: 'block', next: '*' },
+                { blankLine: 'always', prev: '*', next: 'block' },
+                { blankLine: 'always', prev: 'block-like', next: '*' },
+                { blankLine: 'always', prev: '*', next: 'block-like' },
+                { blankLine: 'always', prev: ['import'], next: ['const', 'let', 'var'] }
+            ]
+        }
+    },
+    {
+        files: ['**/*.{js,mjs}'],
+        extends: [eslint.configs.recommended, prettierConfig],
+        languageOptions: {
+            globals: {
+                process: 'readonly'
+            }
+        },
         rules: {
             'no-console': 'error',
             'no-debugger': 'error',
